482bd906ca094edb3cedd9662c8d4ec7979bc1e8
eoquist <emiwemi495@gmail.com>
2023-02-28 23:12:52 +0000

    oh my god pointers

--- main.c
+++ main.c
@@ -7,51 +7,59 @@
 
 int main(int argc, char *argv[])
 {
+	if (argc <= 2){
+		printf("Usage: ./mmmSol <mode> [num threads] <size>");
+		exit(0);
+	}
+
+	char *mode = "X";
+	int numThreads = 0;
+	int dimension = 0;
+	double **matrix_S;
+	double **matrix_P;
+
+	// start: stuff I want to clock
 	double clockstart, clockend;
 	clockstart = rtclock();
 
-	// start: stuff I want to clock
-	int* numThreads = (int *)calloc(1, sizeof(int *));
-	int* dimension = (int *)calloc(1, sizeof(int *));
-	double **matrix;
+	if (strcmp(argv[0], "S") == 0){ // Sequential
+		// Setup
+		strcpy(mode,argv[0]);
+		dimension = atoi(argv[1]);
+		mmm_init(&matrix_S, dimension); // i hate this
 
-	switch (argc)
-	{
-	case 3: // 2 params -- Sequential
-		printf("first param: .%s.\n", argv[0]);
-		printf("second param: .%s.\n", argv[1]);
-		// if (argv[0]){
-		// 	printf("Error: mode must be either S (sequential) or P (parallel)");
-		// }
-		break;
-	case 4: // 3 params -- Parallel
-		break;
-	default: // 0 params, 1 param, or too many params
-		printf("Usage: ./mmmSol <mode> [num threads] <size>");
+	} else if (strcmp(argv[0], "P") == 0){ // Parallel
+		if (argc < 4){
+			printf("Error: parallel mode requires [num threads]\n");
+			exit(0);
+		}
+
+		// Setup
+		strcpy(mode,argv[0]);
+		numThreads = atoi(argv[1]);
+		dimension = atoi(argv[2]);
+		mmm_init(&matrix_S, dimension); // i hate this
+		mmm_init(&matrix_P, dimension); // i hate this so much
+
+	} else {
+		printf("Error: mode must be either S (sequential) or P (parallel)");
 		exit(0);
-		break;
 	}
-
-	// dynamically allocate matrix
-	//
-	//
-	//
-	//
-	//
-	//
-
-	// free
-	for (int i = 0; i < dimension; i++)
-	{
-		free(matrix[i]);
-		matrix[i] = NULL;
-	}
-	free(matrix);
-	matrix = NULL;
 
 	// end: stuff I want to clock
-	clockend = rtclock(); // stop clocking
+	clockend = rtclock();
+	// ========
+	// mode: sequential
+	// thread count: 1
+	// size: 0
+	// ========
 	printf("Time taken: %.6f sec\n", (clockend - clockstart));
+
+	// compare the matrices and then free them
+	// mmm_verify();
+	// mmm_freeup(matrix_S);
+	// mmm_freeup(matrix_P);
+	// exit(0);
 
 	return 0;
 }
